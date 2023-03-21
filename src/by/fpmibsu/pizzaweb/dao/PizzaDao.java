e679270036d4476be624ddd827ced2f117584844
arseni <arseniivankovich0@gmail.com>
2023-03-21 19:39:11 +0300

    Add functions to the interfaces PizzaDao and VacancyDao

--- src/by/fpmibsu/pizzaweb/dao/PizzaDao.java
+++ src/by/fpmibsu/pizzaweb/dao/PizzaDao.java
@@ -2,5 +2,8 @@ package src.by.fpmibsu.pizzaweb.dao;
 
 import src.by.fpmibsu.pizzaweb.entity.Pizza;
 
+import java.util.List;
+
 public interface PizzaDao extends BaseDao<Long, Pizza>{
+    List<Pizza> findInRange (Double lowerBound, Double upperBound);
 }
--- src/by/fpmibsu/pizzaweb/dao/VacancyDao.java
+++ src/by/fpmibsu/pizzaweb/dao/VacancyDao.java
@@ -2,5 +2,9 @@ package src.by.fpmibsu.pizzaweb.dao;
 
 import src.by.fpmibsu.pizzaweb.entity.Vacancy;
 
+import java.util.List;
+
 public interface VacancyDao extends BaseDao<Long, Vacancy> {
+    List<Vacancy> findByTitle(String pattern);
+    List<Vacancy> findBySalary(Double salary);
 }
